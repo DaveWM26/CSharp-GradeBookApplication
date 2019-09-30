85018e2d7aee09d2a2ed0ee439602fedfdff074e
Dave Menham <dave.menham@furnessinternet.com>
2019-09-30 11:28:49 +0100

    Completed module

--- GradeBook/GradeBooks/StandardGradeBook.cs
+++ GradeBook/GradeBooks/StandardGradeBook.cs
@@ -3,8 +3,9 @@ namespace GradeBook.GradeBooks
 {
     public class StandardGradeBook : BaseGradeBook
     {
-        public StandardGradeBook(string name) : base(name)
+        public StandardGradeBook(string name, bool isWeighted) : base(name, isWeighted)
         {
+            IsWeighted = isWeighted;
             Type = Enums.GradeBookType.Standard;
         }
 
